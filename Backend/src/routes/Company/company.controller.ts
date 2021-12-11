66786efa5670d8269a7a34592d9631226c4cc5ac
PatricioLabra <patricio.labra827@gmail.com>
2021-12-11 19:03:45 -0300

    se comenta el encabezado de createCompany

--- Backend/src/routes/Company/company.controller.ts
+++ Backend/src/routes/Company/company.controller.ts
@@ -4,10 +4,10 @@ import Company from './company.model';
 
 
 /**
- * Description
+ * Función encargada de agregar una nueva compañía al sistema. 
  * @route Post '/company/'
- * @param req 
- * @param res 
+ * @param req Request de la petición, se espera que tenga la información de la nueva compañía.
+ * @param res Response, retorna un object con succes: true, data: {_id: ObjectId()}, message: "String" de la nueva compañia si todo sale bien.
  */
 export const createCompany: RequestHandler = async (req, res) => {
 
