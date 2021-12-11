38e789989329a68a5fb2c62f638546bc12fd78ac
PatricioLabra <patricio.labra827@gmail.com>
2021-12-11 19:08:50 -0300

    se comentan los demás encabezados de company

--- Backend/src/routes/Company/company.controller.ts
+++ Backend/src/routes/Company/company.controller.ts
@@ -14,30 +14,30 @@ export const createCompany: RequestHandler = async (req, res) => {
 };
 
 /**
- * Description
+ * Función encargada de actualizar una compañía del sistema en función de los nuevos valores ingresados.
  * @route Put '/company/:id'
- * @param req 
- * @param res 
+ * @param req Request de la petición, se espera que tenga la nueva información de la compañia y su respectivo id por params.
+ * @param res Response, retorna un object con succes: true, data: {_id: ObjectId()}, message: "String" de la compañia actualizada si todo sale bien.
  */
 export const updateCompany: RequestHandler = async (req, res) => {
     
 };
 
 /**
- * Description
+ * Función encargada de obtener una compañía del sistema y retornar los datos de ella para su posterior visualización.
  * @route Get '/company/:id'
- * @param req 
- * @param res 
+ * @param req Request de la petición, se espera que tenga el id de la compañia.
+ * @param res Response, retorna un object con succes: true, data: { company:{} }, message: "String" de la compañia si todo sale bien.
  */
 export const readCompany: RequestHandler = async (req, res) => {
     
 };
 
 /**
- * Description
+ * Función encargada de eliminar una compañía del sistema.
  * @route Delete '/company/:id'
- * @param req 
- * @param res 
+ * @param req equest de la petición, se espera que tenga el id de la compañia.
+ * @param res Response, retorna un object con succes: true, data: { }, message: "String" de la compañia eliminada si todo sale bien.
  */
 export const deleteCompany: RequestHandler = async (req, res) => {
     
