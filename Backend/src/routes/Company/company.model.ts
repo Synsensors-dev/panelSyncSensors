440b22803fa5438aa181cee538aa8318ae51739a
PatricioLabra <patricio.labra827@gmail.com>
2021-12-11 19:00:44 -0300

    se define el model de company

--- Backend/src/routes/Company/company.model.ts
+++ Backend/src/routes/Company/company.model.ts
@@ -1,10 +1,31 @@
 import { Schema, model } from "mongoose";
 
 const companySchema = new Schema({
-
-
-
-
+    name: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    email: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    address: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    representative_name: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    creation_date: {
+        required: true,
+        type: Date,
+        trim: true
+    }
 },{
     versionKey: false,
     timestamps: true
