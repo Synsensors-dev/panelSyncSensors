798dfca876696c98ac9aa1a428ae5fc1f7eca49d
PatricioLabra <patricio.labra827@gmail.com>
2021-12-13 22:27:57 -0300

    se estructura el model de station

--- Backend/src/routes/Station/station.model.ts
+++ Backend/src/routes/Station/station.model.ts
@@ -1,10 +1,46 @@
 import { Schema, model } from "mongoose";
 
 const stationSchema = new Schema({
-
-
-
-
+    name: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    latitude: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    longitude: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    type: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    status: {
+        required: true,
+        type: Boolean,
+        trim: true
+    },
+    location_notes: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    gateway_id: {
+        required: true,
+        type: String,
+        trim: true
+    },
+    company_id: {
+        required: true,
+        type: String,
+        trim: true
+    }
 },{
     versionKey: false,
     timestamps: true
