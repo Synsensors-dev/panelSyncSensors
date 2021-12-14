09bb2f535d653e6dbcd52bad9c6ac560af959093
PatricioLabra <patricio.labra827@gmail.com>
2021-12-13 22:30:54 -0300

    se comentan los encabezados de las funciones

--- Backend/src/routes/Station/station.controller.ts
+++ Backend/src/routes/Station/station.controller.ts
@@ -4,40 +4,40 @@ import Station from './station.model';
 
 
 /**
- * Description
+ * Función encargada de agregar una nueva estación al sistema. 
  * @route Post '/station/'
- * @param req 
- * @param res 
+ * @param req Request de la petición, se espera que tenga la información de la nueva estación.
+ * @param res Response, retorna un object con succes: true, data: {_id: ObjectId()}, message: "String" de la nueva estación si todo sale bien.
  */
 export const createStation: RequestHandler = async (req, res) => {
 
 };
 
 /**
- * Description
+ * Función encargada de actualizar una compañía del sistema en función de los nuevos valores ingresados.
  * @route Put '/station/:id'
- * @param req 
- * @param res 
+ * @param req Request de la petición, se espera que tenga la nueva información de la estación y su respectivo id por params.
+ * @param res Response, retorna un object con succes: true, data: {_id: ObjectId()}, message: "String" de la estación actualizada si todo sale bien.
  */
 export const updateStation: RequestHandler = async (req, res) => {
     
 };
 
 /**
- * Description
+ * Función encargada de obtener una compañía del sistema y retornar los datos de ella para su posterior visualización.
  * @route Get '/station/:id'
- * @param req 
- * @param res 
+ * @param req Request de la petición, se espera que tenga el id de la estación.
+ * @param res Response, retorna un object con succes: true, data: { station:{} }, message: "String" de la estación si todo sale bien.
  */
 export const readStation: RequestHandler = async (req, res) => {
     
 };
 
 /**
- * Description
+ * Función encargada de eliminar una compañía del sistema.
  * @route Delete '/station/:id'
- * @param req 
- * @param res 
+ * @param req equest de la petición, se espera que tenga el id de la estación.
+ * @param res Response, retorna un object con succes: true, data: { }, message: "String" de la estación eliminada si todo sale bien.
  */
 export const deleteStation: RequestHandler = async (req, res) => {
     
