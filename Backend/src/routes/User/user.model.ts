1aa2c1e97118906572abbc03994bfcff29984f8c
PatricioLabra <patricio.labra827@gmail.com>
2021-12-26 16:40:40 -0300

    se agrega el bcrypt a user.model para encriptar la contraseña a guardar

--- Backend/src/routes/User/user.model.ts
+++ Backend/src/routes/User/user.model.ts
@@ -1,4 +1,13 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface IUser extends Document{
+        name: string,
+        email: string,
+        password: string,
+        id_company: string,
+        permission_level: number
+}
 
 const userSchema = new Schema({
     name: {
@@ -9,7 +18,9 @@ const userSchema = new Schema({
     email: {
         required: true, 
         type: String,
-        trim: true
+        trim: true,
+        unique: true, 
+        lowercase: true
     },
     password: {
         required: true,
@@ -31,4 +42,20 @@ const userSchema = new Schema({
     timestamps: true   
 });
 
-export default model('User', userSchema);
+//cifrar contraseña
+userSchema.pre<IUser>( 'save', async function (next) {
+    const user = this;
+    if ( !user.isModified('password') ) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash( user.password, salt );
+    user.password = hash;
+    next();
+});
+
+//comparar contraseñas encriptadas
+userSchema.methods.comparePassword = async function( password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password );
+}
+
+export default model<IUser>('User', userSchema);
