4f75f4e69cc66eb1fb2f9083aad7681e23aa8e27
PatricioLabra <patricio.labra827@gmail.com>
2021-12-26 16:49:34 -0300

    se importa la ruta de user al index

--- Backend/src/routes/index.routes.ts
+++ Backend/src/routes/index.routes.ts
@@ -2,6 +2,7 @@ import {Router} from 'express';
 
 import companyRoutes from './Company/company.routes';
 import stationRoutes from './Station/station.routes';
+import userRoutes from './User/user.routes';
 
 
 const router = Router();
@@ -10,4 +11,4 @@ router.get('/', (req, res) => {
     return res.send('Welcome to my API!');
 });
 
-export default [router, companyRoutes, stationRoutes];
+export default [router, companyRoutes, stationRoutes, userRoutes];
