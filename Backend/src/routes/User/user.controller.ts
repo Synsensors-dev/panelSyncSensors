e0850e5130b5b69ce51d94c392dbaa64b4e5e5f3
PatricioLabra <patricio.labra827@gmail.com>
2021-12-26 15:15:37 -0300

    se estructura lo básico de user.controller, siendo encabezado + función

--- Backend/src/routes/User/user.controller.ts
+++ Backend/src/routes/User/user.controller.ts
@@ -1,2 +1,22 @@
 import { RequestHandler } from "express";
 import User from './user.model';
+
+/**
+ * Funcion que maneja la peticion de agregar un nuevo usuario al sistema
+ * @route Post '/user/signUp'
+ * @param req req Request de la petición, se espera que tenga la información del nuevo usuario
+ * @param res Response, retornará succes: true, data: {token}, message: "String" del nuevo usuario si todo sale bien.
+ */
+ export const signUp: RequestHandler = async (req, res) => {
+    
+}
+
+/**
+ * Funcion que maneja el inicio de sesion de un usuario del sistema
+ * @route Post /user/signin
+ * @param req Request, se espera que tenga la información del usuario
+ * @param res Response, retornará succes: true, data: {token}, message: "String" del usuario si todo sale bien.
+ */
+ export const signIn: RequestHandler = async (req, res) => {
+    
+}
