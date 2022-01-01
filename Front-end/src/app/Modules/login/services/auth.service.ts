83d557d9ae11e926bdef722e8ba27461134615dd
Demitoo <76886434+Demitoo@users.noreply.github.com>
2022-01-01 19:25:36 -0300

    Implementacion token interceptor
    Se implementa token interceptor, el cual añade la cabezera Authorization a todas las request que se hagan, para futuras comprobaciones

--- Front-end/src/app/Modules/login/services/auth.service.ts
+++ Front-end/src/app/Modules/login/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { apiResponse } from '../../shared/interfaces/apiResponse';
 import { loginReq, typeUser } from '../interfaces/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AuthService {
 
   private apiURL= 'http://localhost:4000/'
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private router:Router) { }
 
   /**
    * 
@@ -31,4 +32,29 @@ export class AuthService {
     console.log(loginData)
     return this.http.post<apiResponse>(this.apiURL + 'user/signin',loginData)
   }
+  /**
+   * 
+   * @returns Retorna boolean que indica si hay un usuario logeado o no
+   */
+  loggedIn():boolean{
+    return !!localStorage.getItem('token')
+  }
+
+  /**
+   * 
+   * @returns Retorna token de una sesion iniciada
+   */
+  getToken(){
+    return localStorage.getItem('token')
+  }
+  /**
+   * Desloguea una sesión eliminando el token del local storage
+   */
+
+  logOut(){
+    localStorage.removeItem('token')
+    this.router.navigate(['/login'])
+  }
 }
+
+ 
