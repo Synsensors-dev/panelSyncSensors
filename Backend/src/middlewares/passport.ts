68e8c298056bfee636758ef3e4f3facca56a5b5f
PatricioLabra <patricio.labra827@gmail.com>
2022-01-03 21:53:00 -0300

    se comenta el encabezado de passport

--- Backend/src/middlewares/passport.ts
+++ Backend/src/middlewares/passport.ts
@@ -7,6 +7,10 @@ const opts: StrategyOptions = {
     secretOrKey: config.jwtSecret
 };
 
+/** 
+ * Se encarga de validar el _id que viene dentro del token con los ids registrados en la BD.
+ * @returns null para el error, user (si existe) || null (si no existe)
+ */
 export default new Strategy(opts, async (payload, done) => {
     try {
         const user = await User.findById(payload._id);
