ebfdd63400668eb87217eeed8ea024ca49654dd0
Demitoo <76886434+Demitoo@users.noreply.github.com>
2022-01-05 19:54:34 -0300

    se quitan import no usados en routing.ts

--- Front-end/src/app/app.routing.ts
+++ Front-end/src/app/app.routing.ts
@@ -6,8 +6,6 @@ import { DefaultLayoutComponent } from './containers';
 
 import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
-import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
 
 export const routes: Routes = [
   {
