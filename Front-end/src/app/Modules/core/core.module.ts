135a270252bd2d8825603c9c0372329aecff37da
Demitoo <76886434+Demitoo@users.noreply.github.com>
2022-01-05 19:23:03 -0300

    Iconos para tab, descripción y assets syncsenors
    se agrega lo mencionado

--- Front-end/src/app/Modules/core/core.module.ts
+++ Front-end/src/app/Modules/core/core.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { DefaultLayoutComponent } from './pages/default-layout';
 
 
 @NgModule({
   declarations: [
-    DefaultLayoutComponent
   ],
   imports: [
     CommonModule,
