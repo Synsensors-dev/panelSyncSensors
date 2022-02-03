f8b7564c706213d8d1b31594ccceec6ddd112d8d
Patricio Labra <patricio.labra827@gmail.com>
2022-02-02 23:35:50 -0300

    se finaliza el statusValidator

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -3,12 +3,13 @@ export default {
     jwtSecret: process.env.JWT_SECRET || 'somesecrettoken',
     
     //time
-    ALPHA : 3,                      //variable utilizada para agregar un margen de ingreso a las lecturas 
-    SECONDS_MINUTE : 60,            //variable utilizada para convertir ciertos parametros numericos 
-    MILISECONDS_MINUTE : 60000,     //variable utilizada para convertir ciertos parametros numericos 
-    SECONDS_DAY: 86400,             //variable utilizada para generar token en el endpoint signUp
+    ALPHA : 3,                              //variable utilizada para agregar un margen de ingreso a las lecturas 
+    SECONDS_MINUTE : 60,                    //variable utilizada para convertir ciertos parametros numericos 
+    MILISECONDS_MINUTE : 60000,             //variable utilizada para convertir ciertos parametros numericos 
+    SECONDS_DAY: 86400,                     //variable utilizada para generar token en el endpoint signUp
+    TIME_STATUS_VALIDATOR: "* * * * *",     //variable que indica el margen de tiempo en que se inicia el statusValidator (1 minuto)
 
     //quantity
-    LIMIT_READINGS : 100,           //limite de lecturas a retornar al front en el endpoint sensorReadings
-    LIMIT_ALERTS: 20,               //limite de lecturas a retornar al front en el endpoint recentAlerts
+    LIMIT_READINGS : 100,                   //limite de lecturas a retornar al front en el endpoint sensorReadings
+    LIMIT_ALERTS: 20,                       //limite de lecturas a retornar al front en el endpoint recentAlerts
 }
--- Backend/src/libs/statusValidator.ts
+++ Backend/src/libs/statusValidator.ts
@@ -3,8 +3,6 @@ import Sensor from '../routes/Sensor/sensor.model';
 import config from '../config/config';
 import jwt from 'jsonwebtoken';
 
-const time = "* * * * *"; //1 minuto
-
 /**
  * Función encargada de validar los tokens asociados a cada uno de los sensores de la base de datos. Los cuales mantienen un tiempo
  * de expiración igual a la frecuencia de lectura + un alpha. Cuando dicho token caduca, esta función asume que no se ingresó
@@ -13,9 +11,8 @@ const time = "* * * * *"; //1 minuto
 export const statusValidator =  () => {
 
     //Se estructura el CRON 
-    cron.schedule( time, async () => { 
-        console.log("");
-        console.log("***** Se inicia statusValidator ******");
+    cron.schedule( config.TIME_STATUS_VALIDATOR, async () => { 
+
         const sensors = await Sensor.find();
 
         //recorremos por cada sensor
@@ -23,11 +20,10 @@ export const statusValidator =  () => {
 
             //Se verifica el token
             jwt.verify( sensor.token_reading, config.jwtSecret, async function( err: any ){
-                console.log( "- " + sensor.name + " -> "+ sensor.status);
-                //si expiró o es inválido
+                
+                //si expiró 
                 if ( err  && sensor.status == true ){
 
-                    console.log ( " --- se modifica: "+ sensor.name + " a false  --- ");
                     //se cambia el valor del status en la BD
                     await Sensor.findByIdAndUpdate( sensor._id, { "status": false });
                 }
