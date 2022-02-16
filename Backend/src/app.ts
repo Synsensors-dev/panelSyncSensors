e42a23f887eaed1b978cb05d57238c6fccbf0501
Demitoo <76886434+Demitoo@users.noreply.github.com>
2022-02-16 00:04:10 -0300

    :(

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import passport from 'passport';
@@ -20,22 +21,29 @@ const app = express();
 createRoles();
 statusValidator();
 
+const corsConfig: CorsOptions = {
+    origin: process.env.ORIGIN_FRONT_IP,
+    credentials: true
+};
+
+
+
 // Settings
 app.set('port', process.env.PORT || 4000);
-
-// Middlewares
-app.use(cors())
-app.options('*', cors())
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(passport.initialize());
-passport.use(passportMiddleware);
 
 app.use('/',express.static('client',{redirect:false})) //para produccion
 app.get('*',function(req,res,next){
     return res.sendFile(path.resolve('client/index.html')); //para produccion
 });
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors(corsConfig));
+app.use(passport.initialize());
+passport.use(passportMiddleware);
+
 
 // Routes
 app.use(indexRoutes);
