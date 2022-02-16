5951c8ec4e662baa2491591925a36e02ff65ba43
Demitoo <76886434+Demitoo@users.noreply.github.com>
2022-02-15 23:02:48 -0300

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -19,16 +19,12 @@ import indexRoutes from './routes/index.routes';
 const app = express();
 createRoles();
 statusValidator();
-const corsConfig: CorsOptions = {
-    origin: '*',
-    credentials: true
-};
 
 // Settings
 app.set('port', process.env.PORT || 4000);
 
 // Middlewares
-app.use(cors(corsConfig));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
