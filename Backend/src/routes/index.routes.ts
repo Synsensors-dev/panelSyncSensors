27debde425e58cd03ff685c3b3717fe9fa6bd53d
Demitoo <76886434+Demitoo@users.noreply.github.com>
2022-02-15 22:40:58 -0300

    Update index.routes.ts

--- Backend/src/routes/index.routes.ts
+++ Backend/src/routes/index.routes.ts
@@ -11,6 +11,10 @@ import alertRoutes from './Alert/alert.routes';
 const router = Router();
 
 router.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     return res.send('Welcome to my API!');
 });
 
