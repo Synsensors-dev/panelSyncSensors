575ac395d08c5a561cf5ce5f76c82bfec02732d8
Demitoo <76886434+Demitoo@users.noreply.github.com>
2022-02-15 22:53:58 -0300

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -19,20 +19,6 @@ import indexRoutes from './routes/index.routes';
 const app = express();
 createRoles();
 statusValidator();
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Pass to next layer of middleware
-    next();
-});
 const corsConfig: CorsOptions = {
     origin: '*',
     credentials: true
