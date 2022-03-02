f0c8b170efa4e841dcd0bef75c116c9761567033
Patricio Labra <patricio.labra827@gmail.com>
2022-03-01 22:57:32 -0300

    sew guarda para pushear

--- Backend/src/routes/Station/station.routes.ts
+++ Backend/src/routes/Station/station.routes.ts
@@ -3,13 +3,100 @@ import * as stationCtrl from './station.controller';
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Station:
+ *         type: object
+ *         properties:
+ *              id: 
+ *                  type: string
+ *                  description: id generado por mongodb
+ *              name:
+ *                  type: string
+ *                  description: nombre de la estación
+ *               latitude:
+ *                  type: number
+ *                  description: latitud de la ubicación de la estación
+ *              longitude:
+ *                  type: number
+ *                  description: longitud de la ubicación de la estación
+ *              type: 
+ *                  type: string
+ *                  description: tipo de estación
+ *              status:
+ *                  type: boolean
+ *                  description: estado de la estación 
+ *              location_notes:
+ *                  type: string
+ *                  description: notas de la estación
+ *              id_gateway:
+ *                  type: string
+ *                  description: id del puente
+ *              id_company:
+ *                  type: string
+ *                  description: id de la compañia
+ *          required:
+ *              - name
+ *              - latitude
+ *              - longitude
+ *              - type
+ *              - status
+ *              - location_notes
+ *              - id_gateway
+ *              - id_company
+ *          example:
+ *              name: Estación 1
+ *              latitude: 34
+ *              longitude: 34
+ *              type: Frigorífico
+ *              status: Buena
+ *              location_notes: Ubicada en el subterraneo de la U
+ *              id_gateway: 620dd2bf208318e3b53fbf4e
+ *              id_company: 620dd2bf208318e3b53fbf4e
+ *  parameters:
+ *      stationId:
+ *          in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *              type: string
+ *              description: id de la estación
+ *      companyId:
+ *          in: path
+ *          name: id_company
+ *          required: true
+ *          schema: 
+ *              type: string   
+ *          description: id de la compañia
+ */
+
+
 // Agregar nueva estación
 router.post('/station', stationCtrl.createStation);
 
 // Modificar una estación
 router.put('/station/:id', stationCtrl.updateStation);
 
-// Mostrar una estación
+/**
+ * @swagger
+ * /station/{id}:
+ *  get:
+ *    summary: Obtener los datos de una estación
+ *    tags: [Station]
+ *    parameters:
+ *      - $ref: '#/components/parameters/stationId'
+ *    responses:
+ *      200:
+ *        description: Estación encontrada
+ *        content:
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/Station'
+ *      404:
+ *        description: Error de inexistencia
+ */
 router.get('/station/:id', stationCtrl.readStation);
 
 // Eliminar una estación
