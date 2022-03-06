2ec3f93ebd82ed1af05d5a96f826a9d1f34f7e40
Patricio Labra <patricio.labra827@gmail.com>
2022-03-06 01:56:59 -0300

    se agrega el put de company

--- Backend/src/routes/Company/company.routes.ts
+++ Backend/src/routes/Company/company.routes.ts
@@ -31,11 +31,32 @@ const router = Router();
  *          - address
  *          - representative_name
  *      example:
- *          id: 620dd2bf208318e3b53fbf8i
  *          name: Awa de Uwu
  *          email: awadeuwu@gmail.com
  *          address: calle falsa 1234
  *          representative_name: awa
+ * 
+ *    ObjectReturn: 
+ *      type: object
+ *      properties:
+ *          success:
+ *              type: boolean
+ *              description: variable true o false dependiendo el return
+ *          data:
+ *              type: object
+ *              description: objecto vacío o con valores dependiendo el return
+ *          message: 
+ *              type: string
+ *              description: mensaje del return
+ *      required:
+ *          - success
+ *          - data
+ *          - message
+ *      example:
+ *          success: true
+ *          data: {}
+ *          message: mensaje de la ejecución
+ * 
  *  parameters:
  *      idCompany:
  *          in: path
@@ -53,10 +74,80 @@ const router = Router();
  *  description: Company Endpoints
  */
 
-// Agregar nueva compañía
+/**
+ * @swagger
+ * /company:
+ *  post:
+ *    summary: Agregar una nueva compañia
+ *    tags: [Company]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *          application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/Company'
+ *    responses:
+ *      201:
+ *        description: Compañia encontrada con éxito.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
+ *      404:
+ *        description: La compañia solicitada no existe en el sistema.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
+ *      400:
+ *        description: El id ingresado no es válido.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
+ */
 router.post('/company', companyCtrl.createCompany);
 
-// Modificar una compañía
+/**
+ * @swagger
+ * /company/{id}:
+ *  put:
+ *    summary: Actualizar una compañia a través del ID
+ *    tags: [Company]
+ *    parameters:
+ *      - $ref: '#/components/parameters/idCompany'
+ *    requestBody:
+ *      required: true
+ *      content:
+ *          application/json:
+ *              schema:
+ *                  $ref: '#/components/schemas/Company'
+ *    responses:
+ *      200:
+ *        description: Compañia actualizada de manera correcta.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
+ *      404:
+ *        description: La compañia a modificar no existe en el sistema.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
+ *      400:
+ *        description: El id ingresado no es válido.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
+ *      301:
+ *        description: Los datos a modificar son inválidos.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
+ */
 router.put('/company/:id', companyCtrl.updateCompany);
 
 /**
@@ -73,11 +164,19 @@ router.put('/company/:id', companyCtrl.updateCompany);
  *        content: 
  *          application/json:
  *            schema:
- *            $ref: '#/components/schemas/Company'
+ *            $ref: '#/components/schemas/ObjectReturn'
  *      404:
- *        description: ERROR: La compañia solicitada no existe en el sistema.
+ *        description: La compañia solicitada no existe en el sistema.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
  *      400:
- *        description: ERROR: El id ingresado no es válido.
+ *        description: El id ingresado no es válido.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
  */
 router.get('/company/:id', companyCtrl.readCompany);
 
@@ -92,10 +191,22 @@ router.get('/company/:id', companyCtrl.readCompany);
  *    responses:
  *      200:
  *        description: Compañia eliminada de manera correcta.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
  *      404:
- *        description: ERROR: La compañia solicitada no existe en el sistema.
+ *        description: La compañia solicitada no existe en el sistema.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
  *      400:
- *        description: ERROR: El id ingresado no es válido.
+ *        description: El id ingresado no es válido.
+ *        content: 
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/ObjectReturn'
  */
 router.delete('/company/:id', companyCtrl.deleteCompany);
 
