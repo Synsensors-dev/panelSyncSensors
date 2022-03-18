a5b93a1d0624fb9d4ac17b573ffab0bffd9de1f3
Patricio Labra <patricio.labra827@gmail.com>
2022-03-17 22:37:59 -0300

    se cambia el nombre de una importación

--- Backend/src/libs/initialSetup.ts
+++ Backend/src/libs/initialSetup.ts
@@ -1,6 +1,5 @@
-import { config } from "dotenv";
 import Role from "../models/role.model";
-import configVar from "../config/config";
+import config from "../config/config";
 
 /** 
  * Se encarga de crear los roles de usuarios en la BD. Verificando la existencia de ellos apenas arranca el software.
@@ -16,9 +15,9 @@ export const createRoles = async () => {
 
         // Crea los roles por default
         const values = await Promise.all([
-        new Role({ name: configVar.USER }).save(),
-        new Role({ name: configVar.ADMIN }).save(),
-        new Role({ name: configVar.SUPER_ADMIN }).save()
+        new Role({ name: config.USER }).save(),
+        new Role({ name: config.ADMIN }).save(),
+        new Role({ name: config.SUPER_ADMIN }).save()
         ]);
 
     } catch (error) {
